602b196cf881089f5c75e1151bf61d1f29b28cfa
leo <bulenessdragon@sina.com>
2018-06-15 11:25:12 +0700

    修改房屋买卖合约

--- contracts/BuyingSellingHouses.sol
+++ contracts/BuyingSellingHouses.sol
@@ -1,49 +1,58 @@
 pragma solidity ^0.4.22;
 
 contract BuyingSellingHouses{
-    uint256 houseTotalNum = 1000000;
-    uint256 houseSign = 0;
+
+    address owner;
+    uint256 startSign;
+    uint256 endSign;
+
     struct HouseInfo{
+      string houseToken;
       uint256 sellPrice;
       address sellAddr;
       uint256 commPrice;
-      address commAddr;
       address buyAddr;
       bool isSell;
       bool isCancel;
     }
-    mapping(string => HouseInfo) houseSellInfo;
-    event BuyHouse(address winner,uint256 amount,bool isSells,string houseid);
-    event HouseTotal(uint256 houseSigns);
-    function sellHouse(string _houseid,uint256 _sellPrice,uint256 _commissionPrice,address _commission) public {
-      require(houseSign<houseTotalNum,"1");
-      houseSellInfo[_houseid] = HouseInfo(_sellPrice,msg.sender,_commissionPrice,_commission,_commission,false,false);
-      houseSign = houseSign +1;
-      emit HouseTotal(houseSign);
+    mapping(uint256 => HouseInfo) houseSellInfo;
+
+    constructor(uint256 _startSign,uint256 _endSign) public{
+      owner = msg.sender;
+      startSign = _startSign;
+      endSign = _endSign;
     }
-    function cancelHouse(string _houseid) public {
+
+    function sellHouse(uint256 _houseid,string _houseToken,uint256 _sellPrice,uint256 _commissionPrice) public {
+      require(startSign<=_houseid,"1");
+      require(_houseid<=endSign,"2");
+
+      houseSellInfo[_houseid] = HouseInfo(_houseToken,_sellPrice,msg.sender,_commissionPrice,msg.sender,false,false);
+    }
+    function cancelHouse(uint256 _houseid) public {
       bool tempIsSell = houseSellInfo[_houseid].isSell;
-      require(!tempIsSell,"2");
+      require(!tempIsSell,"3");
       houseSellInfo[_houseid].isCancel = true;
     }
-    function buyHouse(string _houseid) public payable {
+    function buyHouse(uint256 _houseid) public payable
+        require(startSign<=_houseid,"4");
+        require(_houseid<=endSign,"5");
 
         uint256 tempSellPrice = houseSellInfo[_houseid].sellPrice;
         address tempSellAddr = houseSellInfo[_houseid].sellAddr;
         bool tempIsSell = houseSellInfo[_houseid].isSell;
         bool tempIsCancel = houseSellInfo[_houseid].isCancel;
-        require(tempSellAddr != msg.sender,"3");
-        require(msg.sender.balance > tempSellPrice,"4");
-        require(!tempIsSell,"5");
-        require(!tempIsCancel,"6");
+        require(tempSellAddr != msg.sender,"6");
+        require(msg.sender.balance > tempSellPrice,"7");
+        require(!tempIsSell,"8");
+        require(!tempIsCancel,"9");
 
         houseSellInfo[_houseid].buyAddr = msg.sender;
 
         uint256 tempCommissionPrice = houseSellInfo[_houseid].commPrice;
         uint256 tempTruePrice =  tempSellPrice - tempCommissionPrice;
         tempSellAddr.transfer(tempTruePrice);
-        houseSellInfo[_houseid].commAddr.transfer(tempCommissionPrice);
+        owner.transfer(tempCommissionPrice);
         houseSellInfo[_houseid].isSell = true;
-        emit BuyHouse(msg.sender,tempSellPrice,houseSellInfo[_houseid].isSell,_houseid);
     }
 }
