b2df647e6141833e74989624f7f11fb79679df7a
leo <bulenessdragon@sina.com>
2018-08-09 13:48:59 +0800

    code modi

--- contracts/auction.sol
+++ contracts/auction.sol
@@ -157,44 +157,7 @@ contract Auction{
       }
 
     }
-    /* function strConcat(string _a, string _b, string _c, string _d, string _e) internal pure returns (string){
-    bytes memory _ba = bytes(_a);
-    bytes memory _bb = bytes(_b);
-    bytes memory _bc = bytes(_c);
-    bytes memory _bd = bytes(_d);
-    bytes memory _be = bytes(_e);
-    string memory abcde = new string(_ba.length + _bb.length + _bc.length + _bd.length + _be.length);
-    bytes memory babcde = bytes(abcde);
-    uint k = 0;
-    for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-    for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-    for (i = 0; i < _bc.length; i++) babcde[k++] = _bc[i];
-    for (i = 0; i < _bd.length; i++) babcde[k++] = _bd[i];
-    for (i = 0; i < _be.length; i++) babcde[k++] = _be[i];
-    return string(babcde);
-  }
-  function ToString(address x) internal pure returns (string) {
-    bytes memory b = new bytes(20);
-    for (uint i = 0; i < 20; i++)
-        b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
-    return string(b);
-    }
-    function convert(uint256 n) internal pure returns (bytes32) {
-        return bytes32(n);
-    }
-    function bytes32ToString (bytes32 data) internal pure returns (string) {
-        bytes memory bytesString = new bytes(32);
-        for (uint j=0; j<32; j++) {
-            byte char = byte(bytes32(uint(data) * 2 ** (8 * j)));
-            if (char != 0) {
-                bytesString[j] = char;
-            }
-        }
-        return string(bytesString);
-    }
-    function getMaxPriceAndAddress()public view returns(string){
-        return strConcat(bytes32ToString(convert(highestBid)),";",ToString(highestBidder),"","");
-    } */
+    
 
     function getMaxPrice() public view returns(uint256){
         return highestBid;
